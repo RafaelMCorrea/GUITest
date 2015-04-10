40843db1a1ad563e66529c9783f28040c6b405a0
Rafael Correa <rafinha2108@gmail.com>
2015-04-10 18:10:08 -0300

    Configuração de Stage e Table
    Exemplo de configuração de Stage e Table.
    Table possui definição de debug com ShapeRenderer.

--- core/src/br/com/expressobits/games/test/guitest/GUITest.java
+++ core/src/br/com/expressobits/games/test/guitest/GUITest.java
@@ -3,8 +3,6 @@ package br.com.expressobits.games.test.guitest;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
@@ -22,20 +20,43 @@ import com.badlogic.gdx.scenes.scene2d.ui.Table;
 public class GUITest extends ApplicationAdapter {
 	UIWidgetTest uIWidgetTest;
 	
+	//Configurando o Stage
 	private Stage stage;
 	private Table table;
-	
 	private ShapeRenderer shapeRenderer;
 	
 	@Override
 	public void create () {
-		//
-		uIWidgetTest.validate();
+		//uIWidgetTest.validate();
+		
+		//Configurando o Stage
+		stage = new Stage();
+		Gdx.input.setInputProcessor(stage);
+		table = new Table();
+		table.setFillParent(true);//Preenche o tamanho baseado no seu pai(no caso stage)
+		stage.addActor(table);//Adiciona o actor para um pai(no caso stage)
+		shapeRenderer = new ShapeRenderer();//Renderizador utilizado para fazer o debug do draw table
 	}
 
 	@Override
+	public void resize(int width, int height) {
+		stage.getViewport().update(width, height, true);
+	}
+	
+	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		stage.act(Gdx.graphics.getDeltaTime());
+		stage.draw();
+		//Opcional: desenha tabela com debug
+		table.drawDebug(shapeRenderer);
+	}
+	
+	@Override
+	public void dispose() {
+		stage.dispose();
+		shapeRenderer.dispose();
 	}
 }
